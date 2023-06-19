8410283a783a8d80c908daa100d4a0069acee0e9
EMobilio <eymobilio@gmail.com>
2023-06-19 19:17:30 -0400

    updated the board initializer to use dokusan to generate a random puzzle and changed getBlank() to return a single blank position

--- board.py
+++ board.py
@@ -1,10 +1,24 @@
+from dokusan import generators
+
 class Board:
     """ A class for objects representing a 9x9 sudoku board """
 
 
-    """ initializer for an empty 9x9 board with 0s representing blank spaces """
-    def __init__(self):
-        self.cells = [[0] * 9 for row in range(9)]
+    """ Initializer that takes a rank representing the difficulty level
+        and uses dokusan to generate a puzzle of the specified difficulty,
+        copying the puzzle to the cells field. 0 represents an empty space
+    """
+    def __init__(self, rank):
+        # initialize an empty 9x9 2-D list 
+        self.cells = [[""] * 9 for row in range(9)]
+ 
+        # generate a puzzle string with dokusan
+        puzzle = str(generators.random_sudoku(rank))
+
+        # fill in self.cells with corresponding values in the puzzle string
+        for row in range(len(self.cells)):
+            for col in range(len(self.cells[0])):
+                self.cells[row][col] = int(puzzle[len(self.cells)*row + col])
 
     """ method for representing the board object as a sring """
     def __repr__(self):
@@ -26,16 +40,14 @@ class Board:
 
         return s
     
-    """ method that loops through the board's cells to create
-        a list of all of the empty positions in the board
+    """ method that loops through the board's cells to find and return
+        a tuple representing the position of a blank space (or None if
+        the board is complete)
     """
-    def getBlanks(self):
-        blanks = []
-
+    def getBlank(self):
         for row in range(len(self.cells)):
             for col in range(len(self.cells[0])):
                 if self.cells[row][col] == 0:
-                    blanks += [(row, col)]
+                    return (row, col)
 
-        return blanks
-    
+        return None
